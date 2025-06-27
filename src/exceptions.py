5ffbd276cadcb809a4f92051bdbf164e44e1267a
SARATH AC <sarathkunjanu@gmail.com>
2025-06-27 15:18:25 +0530

    logging and exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+def error_message_details(error,error_detail:sys):
+    
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error ocured in the python script[{0}] , Errror occured in the line number[{1}] , Errror message [{2}] ".format(
+       file_name,exc_tb.tb_lineno,str(error)
+    )
+    return error_message
+
+class CustomException:
+    def __init__(self,error_message,error_details:sys):
+        super.__init__(error_message)
+        self.error_message=error_message_details(error_message,error_detail=error_details)
+    def __str__(self):
+       return self.error_message
+   
+   
+if __name__=="__main__"  :
+    try:
+        a=1/0
+        print(a)
+    except Exception as e:
+        logging.info("Divided by Zero error")    
